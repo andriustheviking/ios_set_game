06ab2adae1927287936ab42cbbfc84f93fa84413
kellandrosu <kellandr@oregonstate.edu>
2017-12-19 12:43:36 -0800

    Initial Commit

---
+++ Set Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set Game
+//
+//  Created by Andrius Kelly on 12/19/17.
+//  Copyright © 2017 Andrius Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
